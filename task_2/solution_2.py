dd386c30b9c64c5b4c7d53e688b594a0d554454d
Чумакова Ольга <72072522+esil-11@users.noreply.github.com>
2020-10-24 01:13:54 +0300

    Update solution_2.py
    Странная вышла штука: с некоторыми наборами чисел срабатывает, с некоторыми нет..

--- task_2/solution_2.py
+++ task_2/solution_2.py
@@ -1,4 +1,10 @@
-# your code here
-user_input = input()
-# your code here
-print(most_frequent_character)
+a = input()
+b = 0
+b_ch = ' '
+for i in a:
+    if a.count(i) > b:
+       b = a.count(i)
+    else:
+        pass
+        b_ch = i
+print (b_ch)
