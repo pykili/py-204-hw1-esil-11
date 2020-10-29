fa7b7fcea52721b8de511208edba27bf0c201767
Чумакова Ольга <72072522+esil-11@users.noreply.github.com>
2020-10-29 23:39:00 +0300

    Update solution_2.py

--- task_2/solution_2.py
+++ task_2/solution_2.py
@@ -8,3 +8,18 @@ for i in a:
         pass
         b_ch = i
 print (b_ch)
+
+
+Или:
+
+user_input = input()
+b = 0
+for character in user_input:
+	c = 0
+	for a in user_input:
+		if a == character:
+			c += 1
+	if c > b:
+		b = c
+		most_frequent_character = character
+print(most_frequent_character)
