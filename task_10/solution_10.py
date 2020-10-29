c24740aaee2d425e092a15ecd3ea3f051f6cb4c8
Чумакова Ольга <72072522+esil-11@users.noreply.github.com>
2020-10-30 01:24:36 +0300

    Update solution_10.py

--- task_10/solution_10.py
+++ task_10/solution_10.py
@@ -1,1 +1,10 @@
-# your code here
+a = input()
+n = len(a)
+ans = True
+for i in range(0, n):
+	if a[i] != a[n - i - 1]:
+		ans = False
+if ans == True:
+	print("палиндром")
+else:
+	print("ловушка, это не палиндром")
