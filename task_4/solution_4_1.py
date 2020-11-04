02b21c1fdddd5967550f7365028786dbc5ac37c4
Чумакова Ольга <72072522+esil-11@users.noreply.github.com>
2020-11-05 01:46:13 +0300

    Update solution_4_1.py

--- task_4/solution_4_1.py
+++ task_4/solution_4_1.py
@@ -1,6 +1,26 @@
-# your code here
-for smth in 'a'*10:
-    user_input = input()
-    # your code here
-    if my_cool_condition == True:
-        print(form, lemma)
+for i in 'a'*10:
+	line = input()
+	letter_1 = line[0]
+	for letter_1 in line:
+		if (letter_1 == '#') or (letter_1 == '\t'):
+			pass
+		else:
+			tab = 0
+			id, form, lemma, smth_else = '', '', '', ''
+			for letter in line:
+				if letter == '\t':
+					tab += 1
+				else:
+					if tab == 0:
+						id += letter
+					else:
+						if tab == 1:
+							form += letter
+						else:
+							if tab == 2:
+								lemma += letter
+							else:
+								if tab >= 3:
+									smth_else += letter
+	if form != lemma:
+			print(form, lemma)
